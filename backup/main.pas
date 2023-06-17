2211422e8f920f6163e98cfed35dec2c0fe4152b
Don Wilbrink <don.wilbrink@gmail.com>
2023-06-17 23:21:08 +0200

    Aanpassingen gedaan

--- backup/main.pas
+++ backup/main.pas
@@ -16,9 +16,11 @@ type
     Label1: TLabel;
     Label2: TLabel;
     Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
     MainMenu1: TMainMenu;
-    Memo1: TMemo;
     MenuItem1: TMenuItem;
+    miIngeschreven: TMenuItem;
     miDiagNHoek: TMenuItem;
     miDiagWeb: TMenuItem;
     miMoiree: TMenuItem;
@@ -30,13 +32,16 @@ type
     SpinEdit2: TSpinEdit;
     SpinEdit3: TSpinEdit;
     SpinEdit4: TSpinEdit;
+    SpinEdit5: TSpinEdit;
+    SpinEdit6: TSpinEdit;
     procedure miDiagNHoekClick(Sender: TObject);
     procedure miDriehoekClick(Sender: TObject);
+    procedure miIngeschrevenClick(Sender: TObject);
     procedure miMoireeClick(Sender: TObject);
     procedure miDiagWebClick(Sender: TObject);
     procedure miZeshoekClick(Sender: TObject);
   private
-
+    procedure seVisible;
   public
 
   end;
@@ -56,14 +61,7 @@ var
   a, b, x, y: Array[1..3] of Integer;
 begin
   pbMain.Canvas.Clear;
-  Memo1.Lines.Clear;
-  Label1.Visible:=False;
-  Label2.Visible:=False;
-  Label3.Visible:=False;
-  SpinEdit1.Visible:=False;
-  SpinEdit2.Visible:=False;
-  SpinEdit3.Visible:=False;
-  SpinEdit4.Visible:=False;
+  seVisible;
   a[1] := 6;
   a[2] := 20;
   a[3] := 12;
@@ -77,9 +75,7 @@ begin
     for j:=1 to 3 do
     begin
       x[j] := x0 + k * a[j];
-      Memo1.Lines.Add('x['+IntToStr(j)+']='+IntToStr(x[j]));
       y[j] := y0 + k * b[j];
-      Memo1.Lines.Add('y['+IntToStr(j)+']='+IntToStr(y[j]));
     end;
     pbMain.Canvas.MoveTo(x[1],y[1]);
     pbMain.Canvas.LineTo(x[2],y[2]);
@@ -89,6 +85,46 @@ begin
   until k >= 22;
 end;
 
+procedure TmainForm.miIngeschrevenClick(Sender: TObject);
+var
+  h, j, k, n, n1: Integer;
+  a, b, x, y: Array[1..5] of Integer;
+begin
+  pbMain.Canvas.Clear;
+  seVisible;
+  Label4.Visible:=True;
+  Label5.Visible:=True;
+  SpinEdit5.Visible:=True;
+  SpinEdit6.Visible:=True;
+  h := pbMain.Height-40;
+  x[1] := 40; x[2] := h; x[3] := h; x[4] := 40; x[5] := 40;
+  y[1] := 40; y[2] := 40; y[3] := h; y[4] := h; y[5] := 40;
+  k := SpinEdit5.Value;
+  n1 := SpinEdit6.Value;
+  for n := 1 to n1 do
+  begin
+    pbMain.Canvas.MoveTo(x[1],y[1]);
+    for j := 2 to 4 do
+      pbMain.Canvas.LineTo(x[j],y[j]);
+    pbMain.Canvas.LineTo(x[1],y[1]);
+    for j := 1 to 4 do
+    begin
+      a[j] := x[j] + Trunc((x[j+1]-x[j])/k);
+      b[j] := y[j] + Trunc((y[j+1]-y[j])/k);
+    end;
+    for j := 1 to 4 do
+    begin
+      x[j] := a[j];
+      y[j] := b[j];
+    end;
+    for j := 1 to 4 do
+    begin
+      x[5] := x[1];
+      y[5] := y[1];
+    end;
+  end;
+end;
+
 procedure TmainForm.miDiagNHoekClick(Sender: TObject);
 var
   a, b, n, i, j, u, v: Integer;
@@ -96,14 +132,12 @@ var
   x, y: Array of Integer;
 begin
   pbMain.Canvas.Clear;
-  Memo1.Lines.Clear;
+  seVisible;
   Label1.Visible:=True;
   Label2.Visible:=True;
-  Label3.Visible:=False;
   SpinEdit1.Visible:=True;
   SpinEdit2.Visible:=True;
   SpinEdit3.Visible:=True;
-  SpinEdit4.Visible:=False;
   a := SpinEdit1.Value;
   b := SpinEdit2.Value;
   n := SpinEdit3.Value;
@@ -129,26 +163,20 @@ end;
 
 procedure TmainForm.miMoireeClick(Sender: TObject);
 var
-  a, j: Integer;
+  a, h, j: Integer;
 begin
   pbMain.Canvas.Clear;
-  Memo1.Lines.Clear;
-  Label1.Visible:=False;
-  Label2.Visible:=False;
-  Label3.Visible:=False;
-  SpinEdit1.Visible:=False;
-  SpinEdit2.Visible:=False;
-  SpinEdit3.Visible:=False;
-  SpinEdit4.Visible:=False;
+  seVisible;
+  h := pbMain.Height-10;
   for j := 0 to 50 do
   begin
     a := j * 10;
     with pbMain.Canvas do
     begin
-      MoveTo(0,500);
+      MoveTo(0,h);
       LineTo(a,0);
-      MoveTo(0,500);
-      LineTo(500,a);
+      MoveTo(0,h);
+      LineTo(h,a);
     end;
   end;
   for j := 0 to 50 do
@@ -156,10 +184,10 @@ begin
     a := j * 10;
     with pbMain.Canvas do
     begin
-      MoveTo(500,0);
+      MoveTo(h,0);
       LineTo(0,a);
-      MoveTo(500,0);
-      LineTo(a,500);
+      MoveTo(h,0);
+      LineTo(a,h);
     end;
   end;
 end;
@@ -169,13 +197,8 @@ var
   a, b, i, j, n, y1, y2: Integer;
 begin
   pbMain.Canvas.Clear;
-  Memo1.Lines.Clear;
-  Label1.Visible:=False;
-  Label2.Visible:=False;
+  seVisible;
   Label3.Visible:=True;
-  SpinEdit1.Visible:=False;
-  SpinEdit2.Visible:=False;
-  SpinEdit3.Visible:=False;
   SpinEdit4.Visible:=True;
   y1 := 0;
   y2 := pbMain.Height-5;
@@ -201,14 +224,7 @@ var
   h, w, w1: real;
 begin
   pbMain.Canvas.Clear;
-  Memo1.Lines.Clear;
-  Label1.Visible:=False;
-  Label2.Visible:=False;
-  Label3.Visible:=False;
-  SpinEdit1.Visible:=False;
-  SpinEdit2.Visible:=False;
-  SpinEdit3.Visible:=False;
-  SpinEdit4.Visible:=False;
+  seVisible;
   u := 200;
   v := 200;
   r := 200;
@@ -239,6 +255,21 @@ begin
       y[j] := b[j];
     end;
   end;
+end;
+
+procedure TmainForm.seVisible;
+begin
+  Label1.Visible:=False;
+  Label2.Visible:=False;
+  Label3.Visible:=False;
+  Label4.Visible:=False;
+  Label5.Visible:=False;
+  SpinEdit1.Visible:=False;
+  SpinEdit2.Visible:=False;
+  SpinEdit3.Visible:=False;
+  SpinEdit4.Visible:=False;
+  SpinEdit5.Visible:=False;
+  SpinEdit6.Visible:=False;
 end;
 
 end.
--- main.pas
+++ main.pas
@@ -16,8 +16,11 @@ type
     Label1: TLabel;
     Label2: TLabel;
     Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
     MainMenu1: TMainMenu;
     MenuItem1: TMenuItem;
+    miIngeschreven: TMenuItem;
     miDiagNHoek: TMenuItem;
     miDiagWeb: TMenuItem;
     miMoiree: TMenuItem;
@@ -29,13 +32,16 @@ type
     SpinEdit2: TSpinEdit;
     SpinEdit3: TSpinEdit;
     SpinEdit4: TSpinEdit;
+    SpinEdit5: TSpinEdit;
+    SpinEdit6: TSpinEdit;
     procedure miDiagNHoekClick(Sender: TObject);
     procedure miDriehoekClick(Sender: TObject);
+    procedure miIngeschrevenClick(Sender: TObject);
     procedure miMoireeClick(Sender: TObject);
     procedure miDiagWebClick(Sender: TObject);
     procedure miZeshoekClick(Sender: TObject);
   private
-
+    procedure seVisible;
   public
 
   end;
@@ -55,13 +61,7 @@ var
   a, b, x, y: Array[1..3] of Integer;
 begin
   pbMain.Canvas.Clear;
-  Label1.Visible:=False;
-  Label2.Visible:=False;
-  Label3.Visible:=False;
-  SpinEdit1.Visible:=False;
-  SpinEdit2.Visible:=False;
-  SpinEdit3.Visible:=False;
-  SpinEdit4.Visible:=False;
+  seVisible;
   a[1] := 6;
   a[2] := 20;
   a[3] := 12;
@@ -85,6 +85,46 @@ begin
   until k >= 22;
 end;
 
+procedure TmainForm.miIngeschrevenClick(Sender: TObject);
+var
+  h, j, k, n, n1: Integer;
+  a, b, x, y: Array[1..5] of Integer;
+begin
+  pbMain.Canvas.Clear;
+  seVisible;
+  Label4.Visible:=True;
+  Label5.Visible:=True;
+  SpinEdit5.Visible:=True;
+  SpinEdit6.Visible:=True;
+  h := pbMain.Height-40;
+  x[1] := 40; x[2] := h; x[3] := h; x[4] := 40; x[5] := 40;
+  y[1] := 40; y[2] := 40; y[3] := h; y[4] := h; y[5] := 40;
+  k := SpinEdit5.Value;
+  n1 := SpinEdit6.Value;
+  for n := 1 to n1 do
+  begin
+    pbMain.Canvas.MoveTo(x[1],y[1]);
+    for j := 2 to 4 do
+      pbMain.Canvas.LineTo(x[j],y[j]);
+    pbMain.Canvas.LineTo(x[1],y[1]);
+    for j := 1 to 4 do
+    begin
+      a[j] := x[j] + Trunc((x[j+1]-x[j])/k);
+      b[j] := y[j] + Trunc((y[j+1]-y[j])/k);
+    end;
+    for j := 1 to 4 do
+    begin
+      x[j] := a[j];
+      y[j] := b[j];
+    end;
+    for j := 1 to 4 do
+    begin
+      x[5] := x[1];
+      y[5] := y[1];
+    end;
+  end;
+end;
+
 procedure TmainForm.miDiagNHoekClick(Sender: TObject);
 var
   a, b, n, i, j, u, v: Integer;
@@ -92,13 +132,12 @@ var
   x, y: Array of Integer;
 begin
   pbMain.Canvas.Clear;
+  seVisible;
   Label1.Visible:=True;
   Label2.Visible:=True;
-  Label3.Visible:=False;
   SpinEdit1.Visible:=True;
   SpinEdit2.Visible:=True;
   SpinEdit3.Visible:=True;
-  SpinEdit4.Visible:=False;
   a := SpinEdit1.Value;
   b := SpinEdit2.Value;
   n := SpinEdit3.Value;
@@ -124,36 +163,31 @@ end;
 
 procedure TmainForm.miMoireeClick(Sender: TObject);
 var
-  a, j: Integer;
+  a, h, j: Integer;
 begin
   pbMain.Canvas.Clear;
-  Label1.Visible:=False;
-  Label2.Visible:=False;
-  Label3.Visible:=False;
-  SpinEdit1.Visible:=False;
-  SpinEdit2.Visible:=False;
-  SpinEdit3.Visible:=False;
-  SpinEdit4.Visible:=False;
-  for j := 0 to 50 do
+  seVisible;
+  h := pbMain.Height-10;
+  for j := 0 to Trunc(h/10) do
   begin
     a := j * 10;
     with pbMain.Canvas do
     begin
-      MoveTo(0,500);
+      MoveTo(0,h);
       LineTo(a,0);
-      MoveTo(0,500);
-      LineTo(500,a);
+      MoveTo(0,h);
+      LineTo(h,a);
     end;
   end;
-  for j := 0 to 50 do
+  for j := 0 to Trunc(h/10) do
   begin
     a := j * 10;
     with pbMain.Canvas do
     begin
-      MoveTo(500,0);
+      MoveTo(h,0);
       LineTo(0,a);
-      MoveTo(500,0);
-      LineTo(a,500);
+      MoveTo(h,0);
+      LineTo(a,h);
     end;
   end;
 end;
@@ -163,12 +197,8 @@ var
   a, b, i, j, n, y1, y2: Integer;
 begin
   pbMain.Canvas.Clear;
-  Label1.Visible:=False;
-  Label2.Visible:=False;
+  seVisible;
   Label3.Visible:=True;
-  SpinEdit1.Visible:=False;
-  SpinEdit2.Visible:=False;
-  SpinEdit3.Visible:=False;
   SpinEdit4.Visible:=True;
   y1 := 0;
   y2 := pbMain.Height-5;
@@ -194,13 +224,7 @@ var
   h, w, w1: real;
 begin
   pbMain.Canvas.Clear;
-  Label1.Visible:=False;
-  Label2.Visible:=False;
-  Label3.Visible:=False;
-  SpinEdit1.Visible:=False;
-  SpinEdit2.Visible:=False;
-  SpinEdit3.Visible:=False;
-  SpinEdit4.Visible:=False;
+  seVisible;
   u := 200;
   v := 200;
   r := 200;
@@ -231,6 +255,21 @@ begin
       y[j] := b[j];
     end;
   end;
+end;
+
+procedure TmainForm.seVisible;
+begin
+  Label1.Visible:=False;
+  Label2.Visible:=False;
+  Label3.Visible:=False;
+  Label4.Visible:=False;
+  Label5.Visible:=False;
+  SpinEdit1.Visible:=False;
+  SpinEdit2.Visible:=False;
+  SpinEdit3.Visible:=False;
+  SpinEdit4.Visible:=False;
+  SpinEdit5.Visible:=False;
+  SpinEdit6.Visible:=False;
 end;
 
 end.
